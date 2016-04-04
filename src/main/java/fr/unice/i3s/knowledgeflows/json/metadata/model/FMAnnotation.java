7b478f3dba48af1b8649ec16a97a22538c62efcc
Simon Urli <simon.urli@gmail.com>
2016-04-04 17:09:05 +0200

    Define the root for JSON file

--- src/main/java/fr/unice/i3s/knowledgeflows/json/metadata/model/FMAnnotation.java
+++ src/main/java/fr/unice/i3s/knowledgeflows/json/metadata/model/FMAnnotation.java
@@ -1,8 +1,26 @@
 package fr.unice.i3s.knowledgeflows.json.metadata.model;
 
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by urli on 04/04/2016.
  */
+@XmlRootElement
 public class FMAnnotation {
 
+    private List<FeatureAnnotation> features;
+
+    public FMAnnotation() {
+        features = new ArrayList<FeatureAnnotation>();
+    }
+
+    public List<FeatureAnnotation> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(List<FeatureAnnotation> features) {
+        this.features = features;
+    }
 }
